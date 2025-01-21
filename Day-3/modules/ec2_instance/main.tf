df3c016d778fdb2b528eb175d3206e1c799d414c
bhavitharepository <reddyvenkatrami22@gmail.com>
2025-01-21 15:08:01 +0530

    Update main.tf
    more data

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -1,3 +1,16 @@
+[variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"              note:here the variables values declarating the providers. its check the 14 line below attached var.]
+}
+
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+}]
+
+
 provider "aws" {
   region = "us-east-1"
 }
